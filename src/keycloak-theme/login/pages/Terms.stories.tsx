4fe6ddeab5ec3ac6b2418cfb1bf76f3b93367e56
Alva A <rpondering@gmail.com>
2024-02-19 04:44:44 +0700

    feat: prettier config

--- src/keycloak-theme/login/pages/Terms.stories.tsx
+++ src/keycloak-theme/login/pages/Terms.stories.tsx
@@ -1,18 +1,18 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from "@storybook/react";
 import { createPageStory } from "../createPageStory";
 
 const { PageStory } = createPageStory({
-    pageId: "terms.ftl"
+  pageId: "terms.ftl",
 });
 
 const meta = {
-    title: "login/Terms",
-    component: PageStory,
+  title: "login/Terms",
+  component: PageStory,
 } satisfies Meta<typeof PageStory>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-    render: () => <PageStory />
+  render: () => <PageStory />,
 };
--- src/main.tsx
+++ src/main.tsx
@@ -6,30 +6,27 @@ import { kcContext as kcAccountThemeContext } from "./keycloak-theme/account/kcC
 const KcLoginThemeApp = lazy(() => import("./keycloak-theme/login/KcApp"));
 const KcAccountThemeApp = lazy(() => import("./keycloak-theme/account/KcApp"));
 // Important note:
-// In this starter example we show how you can have your react app and your Keycloak theme in the same repo.  
-// Most Keycloakify user only want to great a Keycloak theme.  
-// If this is your case run the few commands that will remover everything that is not strictly related to the 
+// In this starter example we show how you can have your react app and your Keycloak theme in the same repo.
+// Most Keycloakify user only want to great a Keycloak theme.
+// If this is your case run the few commands that will remover everything that is not strictly related to the
 //Keycloak theme:
 // https://github.com/keycloakify/keycloakify-starter?tab=readme-ov-file#i-only-want-a-keycloak-theme
 const App = lazy(() => import("./App"));
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <Suspense>
-            {(()=>{
+  <StrictMode>
+    <Suspense>
+      {(() => {
+        if (kcLoginThemeContext !== undefined) {
+          return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
+        }
 
-                if( kcLoginThemeContext !== undefined ){
-                    return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
-                }
+        if (kcAccountThemeContext !== undefined) {
+          return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
+        }
 
-                if( kcAccountThemeContext !== undefined ){
-                    return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
-                }
-
-                return <App />;
-
-            })()}
-        </Suspense>
-    </StrictMode>
+        return <App />;
+      })()}
+    </Suspense>
+  </StrictMode>
 );
-
